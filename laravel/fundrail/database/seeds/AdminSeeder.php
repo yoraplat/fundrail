16329b6627b3a55fbba8f8da315581086d37fd03
Yoram <yoraplat@arteveldehs.be>
2019-04-25 12:39:09 +0200

    added admin seeder

---
+++ laravel/fundrail/database/seeds/AdminSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => "admin",
+            'email' => "admin@fundrail.com",
+            'role_type' => "1",
+            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        ]);
+    }
+
+}
--- laravel/fundrail/database/seeds/DatabaseSeeder.php
+++ laravel/fundrail/database/seeds/DatabaseSeeder.php
@@ -19,6 +19,13 @@ class DatabaseSeeder extends Seeder
         DB::table('categories')->insert([
             'name' => "Tools",
         ]);
+
+        DB::table('users')->insert([
+            'name' => "admin",
+            'email' => "admin@fundrail.com",
+            'role_type' => "1",
+            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        ]);
     }
 
     public function down()
