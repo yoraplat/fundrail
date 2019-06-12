edd29135791f3856e11b19af814bfd76d3ae9323
yorapalt <yoraplat@student.arteveldehs.be>
2019-06-12 16:48:57 +0200

    added news to nav

--- laravel/fundrail/resources/views/includes/nav.blade.php
+++ laravel/fundrail/resources/views/includes/nav.blade.php
@@ -32,6 +32,9 @@
       <li class="nav-item">
         <a class="nav-link" href="/projects">Projects</a>
       </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/news">News</a>
+      </li>
       @if (Auth::check())
       <li class="nav-item">
         <a class="nav-link" href="/dashboard">Dashboard</a>
--- laravel/fundrail/resources/views/pages/post.blade.php
+++ laravel/fundrail/resources/views/pages/post.blade.php
@@ -5,11 +5,12 @@
     @include('includes.nav')
         <div class="container" id="showcase-container">
             <h2>{{ $post->title }}</h2>
+            <small class="ml-2">{{ $post->created_at }}</small>
+            
             <div class="border">
                 <div class="row">
                     <div class="col-12"><h3>{{ $post->content }}</h3></div>
-                    <div class="col-12"><p>{{ $post->created_at }}</p></div>
-                    <div class="col-12"><img src="{{ asset('storage/') . $post->image_path }}" alt=""></div>
+                    <div class="col-12"><img class="postImg" src="{{ asset('storage/') . $post->image_path }}" alt=""></div>
                 </div>
             </div>
         </div>
